a277346580a0ea43dd5cb954d3460ee92ae79fb6
yapisango <yapisango@gmail.com>
2024-09-29 23:31:59 +0200

    just updated on vscode
    just updatedfrom vscode

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,36 @@
+const path = require('path');
+
 module.exports = {
-  "output": {
-    "filename": "[name].pack.js"
-  },
-  "resolve": {
-    "extensions": [
-      ".js",
-      ".json"
-    ],
-    "alias": {}
-  },
-  "module": {
-    "rules": [
-      {
-        "use": {
-          "loader": "babel-loader",
-          "options": {
-            "presets": [
-              "babel-preset-env",
-              "babel-preset-react"
-            ]
-          }
-        },
-        "exclude": /node_modules/,
-        "test": /\.js$/
-      }
-    ]
-  },
-  "entry": {
-    "index": "./index"
-  }
-}
+    entry: './index.js', 
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    devServer: {
+        static: path.join(__dirname, 'dist'), // Changed from contentBase to static
+        compress: true,
+        port: 9000,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+};
+
